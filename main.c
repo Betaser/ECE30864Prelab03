b2b032af6fa376658e6772e16c9ee8d0d34c9559
Betaser <tylerharrisonhsieh@gmail.com>
2026-02-01 23:16:06 -0500

    CSP-5 Render player

--- main.c
+++ main.c
@@ -3,10 +3,18 @@
 int main() {
 	InitWindow(800, 600, "Gaem");
 
+	int player_x, player_y;
+	player_x = 200;
+	player_y = 500;
+
 	while (!WindowShouldOpen()) {
 		BeginDrawing();
 
+		// Ground
 		DrawRectangle(0, 400, 800, 200, BROWN);
+
+		// Player
+		DrawRectangle(player_x, player_y, 100, 100, WHITE);
 		EndDrawing();
 	}
 }
